54de5984c307a8e318430aa7316ad1d82636396e
Peyton <peyton.urquhart@wsu.edu>
2020-03-29 20:57:32 -0700

    Changed rectangle->rectangularprism

---
+++ ShapesProgram/ShapesProgram/RectangularPrism.cs
@@ -0,0 +1,25 @@
+﻿namespace ShapesProgram
+{
+    internal class RectangularPrism
+    {
+        private double length;
+
+        private double width;
+
+        private double height;
+
+        public RectangularPrism(double l, double w, double h)
+        {
+            this.length = l;
+
+            this.width = w;
+
+            this.height = h;
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * this.width * this.length + this.height * this.length + this.height * this.width;
+        }
+    }
+}
