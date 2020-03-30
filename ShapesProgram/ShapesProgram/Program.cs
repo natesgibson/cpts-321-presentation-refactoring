06b5d3c174862bb2e4d772d70e912be392e0e92f
Odeysiuss Tuon <odeysiuss.tuon@wsu.edu>
2020-03-29 18:37:25 -0700

    Added basic structure for ShapesHandler and Program

--- ShapesProgram/ShapesProgram/Program.cs
+++ ShapesProgram/ShapesProgram/Program.cs
@@ -1,12 +1,20 @@
 ﻿namespace ShapesProgram
 {
     using System;
+    using System.IO;
     
     public class Program
     {
         public static void Main(string[] args)
         {
-            
+            ShapesHandler shapesHandler = new ShapesHandler();
+
+            using (FileStream file = new FileStream("shapes_data.csv", FileMode.Open, FileAccess.Read))
+            {
+                shapesHandler.ParseShapes(new StreamReader(file));
+
+                Console.WriteLine("Sum of area of all shapes: " + shapesHandler.GetTotalArea());
+            }
         }
     }
 }
--- ShapesProgram/ShapesProgram/ShapesHandler.cs
+++ ShapesProgram/ShapesProgram/ShapesHandler.cs
@@ -1,7 +1,28 @@
 ﻿namespace ShapesProgram
 {
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+
     internal class ShapesHandler
     {
+        List<object> shapes;
+
+        public ShapesHandler()
+        {
+            this.shapes = new List<object>();
+        }
+
+        // Gets the total area of the shapes collection
+        public double GetTotalArea()
+        {
+            throw new NotImplementedException();
+        }
         
+        // Parse CSV files
+        public void ParseShapes(TextReader text)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
