3078ac94835451d2df282b347daf1653ece17580
Peyton <peyton.urquhart@wsu.edu>
2020-03-29 20:09:46 -0700

    Finished unRefactored Square and Circle

--- ShapesProgram/ShapesProgram/Circle.cs
+++ ShapesProgram/ShapesProgram/Circle.cs
@@ -2,6 +2,16 @@
 {
     internal class Circle
     {
+        private double radius;
 
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public double GetArea()
+        {
+            return 3.1415926535897931d * (this.radius * this.radius);
+        }
     }
 }
--- ShapesProgram/ShapesProgram/Square.cs
+++ ShapesProgram/ShapesProgram/Square.cs
@@ -2,6 +2,16 @@
 {
     internal class Square
     {
-        
+        private double sideLength;
+
+        public Square(double sideLength)
+        {
+            this.sideLength = sideLength;
+        }
+
+        public double GetArea()
+        {
+            return (this.sideLength * this.sideLength);
+        }       
     }
 }
